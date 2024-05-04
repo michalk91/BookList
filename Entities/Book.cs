d04627de38faf376c87ffef4612844f2cfcaab6f
Michal Kukla <michal.kukla91@gmail.com>
2024-05-04 20:45:02 +0200

    add book entity

---
+++ Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookList.Entities
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+    }
+}
