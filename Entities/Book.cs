501ff7341df1ba6ac9c646bc17aabc51e8828719
Michal Kukla <michal.kukla91@gmail.com>
2024-05-04 20:47:52 +0200

    add BsonRepresentations attributes

--- Entities/Book.cs
+++ Entities/Book.cs
@@ -7,7 +7,9 @@ namespace BookList.Entities
         [BsonId]
         [BsonElement("_id"), BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string? Id { get; set; }
+        [BsonElement("_title"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
         public string? Title { get; set; }
+        [BsonElement("_author"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
         public string? Author { get; set; }
     }
 }
