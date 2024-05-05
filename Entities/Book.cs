8f6f9191c187fe05db3ea4e1651254462cce598b
Michal Kukla <michal.kukla91@gmail.com>
2024-05-05 17:12:04 +0200

    purify the code

--- Entities/Book.cs
+++ Entities/Book.cs
@@ -1,15 +1,16 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace BookList.Entities
 {
     public class Book
     {
         [BsonId]
-        [BsonElement("_id"), BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
-        [BsonElement("_title"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        [BsonElement("_title"), BsonRepresentation(BsonType.String)]
         public string? Title { get; set; }
-        [BsonElement("_author"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        [BsonElement("_author"), BsonRepresentation(BsonType.String)]
         public string? Author { get; set; }
     }
 }
